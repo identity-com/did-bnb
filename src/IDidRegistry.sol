3157f56dc40ca00e8458fbd7a9ee3fec172af5e5
Robert Leonard <rleonard@Roberts-MBP.attlocal.net>
2023-08-07 19:00:03 -0400

    remove unused events

--- src/IDidRegistry.sol
+++ src/IDidRegistry.sol
@@ -2,25 +2,7 @@
 
 pragma solidity ^0.8.19;
 interface IDidRegistry {
-    event DIDOwnerChanged(
-        address indexed identity,
-        address owner,
-        uint previousChange
-    );
-
-    event DIDDelegateChanged(
-        address indexed identity,
-        bytes32 delegateType,
-        address delegate,
-        uint validTo,
-        uint previousChange
-    );
-
-    event DIDAttributeChanged(
-        address indexed identity,
-        bytes32 name,
-        bytes value,
-        uint validTo,
-        uint previousChange
-    );
+    /**
+        - Events from Update and Delete operations will live here
+     */
 }
