81b71ab7d383b406708e04e0dde410e5682f01bf
Robert Leonard <rleonard@Roberts-MacBook-Pro.local>
2023-08-11 09:22:27 -0400

    Removed comments

--- test/DidRegistryTest.sol
+++ test/DidRegistryTest.sol
@@ -18,20 +18,3 @@ contract DidRegistryTest is Test {
         didRegistry.addVerificationMethod(user, newVm);
     }
 }
-
-/**
-Invariants of the system:
-- a didState should always have at least 1 verification method
-- a verification method that is protected can not be removed
-- a verification with an ownership flag can only be edited by the owner of the vm? Need clarity
-- only the did owner can add verification methods
-
-
-Questions:
-1) What does the ownership flag do exactly?
-2) Can any of the controllers update verification methods?
-3) What can the other authority keys update?
-4) What does the protected flag mean and when updating verification flags what are the conditions? In the rust contract it is `has_authority_verification_methods`
-5) What flags can a user update on their verificationFlags that they own (ie they own the address in keyData)
-6) Are controller explicitly added or are they derived from verification methods?
- */
