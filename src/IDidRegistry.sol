5977a53916c316cc930e7a0736e86e0a5da410ca
Robert Leonard <rleonard@Roberts-MBP.attlocal.net>
2023-08-15 09:27:03 -0400

    Fix floating pragma

--- src/IDidRegistry.sol
+++ src/IDidRegistry.sol
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: MIT */
 
-pragma solidity ^0.8.19;
+pragma solidity 0.8.19;
 interface IDidRegistry {
     /**
         - Events from Update and Delete operations will live here
