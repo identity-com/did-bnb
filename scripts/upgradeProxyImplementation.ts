f186f8032e080fc08368b8e964fb6d5893a6b550
Robert Leonard <rleonard@Roberts-MBP.attlocal.net>
2023-09-12 16:35:23 -0400

    Add script to create new implementation contract and create admin proposal

--- scripts/upgradeProxyImplementation.ts
+++ scripts/upgradeProxyImplementation.ts
@@ -1,11 +1,18 @@
-import { ethers, upgrades } from "hardhat";
+import { ethers, defender } from "hardhat";
+import { verify, sleep, loadRelayerSigner } from "./util";
 
 async function main() {
-  const didRegistryContract = await ethers.getContractFactory("DIDRegistry");
-  const deployment = await upgrades.upgradeProxy(process.env.DID_REGISTRY_IMPLEMENTATION_ADDRESS!, didRegistryContract);
-  await deployment.waitForDeployment();
-  console.log(await deployment.getAddress());
+  const relaySigner = await loadRelayerSigner();
+
+  //@ts-ignore
+  const didRegistryContract = await ethers.getContractFactory("DIDRegistry", relaySigner);
+  const proposalResponse = await defender.proposeUpgrade(process.env.DID_REGISTRY_PROXY_ADDRESS!, didRegistryContract, {
+    multisig: process.env.GNOSIS_ADDRESS!,
+    kind: 'uups',
+    redeployImplementation: 'always'
+  });
 }
+
 
 
 main().catch((error) => {
